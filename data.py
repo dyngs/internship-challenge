8717ce9516dc4d3d524c47943c8959d92d33fa16
dyngs <d.skotarczak@ufl.edu>
2020-08-31 18:45:53 -0500

    Task 1 completed.

--- data.py
+++ data.py
@@ -19,3 +19,24 @@ class ParentProduct:
 
     def show_name(self):
         print(f"Name of this product is {self.name}")
+
+class Product(ParentProduct):
+    def __init__(self, name, amount, price):
+        super().__init__(name)
+        self.amount = amount
+        self.price = price
+
+    def show_price(self, currency: str):
+        print(self.name + "price is" + str(self.price))
+
+    def show_amount(self):
+        print(self.name + "amount is" + str(self.amount))
+
+    def calculate_total_value(self):
+        return self.amount*self.price
+
+    def get_name(self):
+        return self.name
+
+
+
